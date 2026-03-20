501f185b47ac242c97a4c8500882e53c5b4617a1
LukasHlobil1 <lukas.hlobil@orbiszlin.cz>
2026-03-20 12:41:02 +0100

    Úprava konfigurace Vite pro odstranění PWA pluginu a zjednodušení nastavení

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['icon-192.png', 'icon-512.png'],
-      manifest: false,
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/api\.smartcar\.com\/.*/i,
-            handler: 'NetworkFirst',
-            options: {
-              cacheName: 'smartcar-api',
-              expiration: {
-                maxEntries: 50,
-                maxAgeSeconds: 300
-              }
-            }
-          }
-        ]
-      }
-    })
-  ]
-});
+  plugins: [react()],
+})

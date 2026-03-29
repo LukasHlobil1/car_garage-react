79003c96f9c7c53462b182215a2894f0b63419d6
LukasHlobil1 <lukas.hlobil@orbiszlin.cz>
2026-03-29 22:19:29 +0200

    smazané pwa

--- vite.config.js
+++ vite.config.js
@@ -5,30 +5,6 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'robots.txt'],
-      manifest: {
-        name: 'Car Garage',
-        short_name: 'Garage',
-        description: 'Moje garáž aplikace',
-        theme_color: '#000000',
-        background_color: '#000000',
-        display: 'standalone',
-        start_url: '/',
-        icons: [
-          {
-            src: '/icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/icon-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
+
   ]
 })
